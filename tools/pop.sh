78fe7f66db142060d8d242cead7f86fa2446cc9d
Andy Armstrong <andy@hexten.net>
2015-03-12 09:30:49 +0000

    Try lots of permutations.

--- tools/pop.sh
+++ tools/pop.sh
@@ -2,30 +2,37 @@
 
 DURATION=240
 
+outsize="1920x1080"
+
 for src in "$@"; do
-  for gain in 1 2 4 8 16; do
+  for gain in 1 4 16; do
     for chans in 'y' 'yuv'; do
-      for permute in 'zigzag' 'raster' 'weave'; do
+      for size in 64 128 256 512; do
+        for permute in 'zigzag' 'raster' 'weave'; do
 
-        tag="$permute-$gain-$chans"
-        dst="$src.$tag.mov"
+          tag="$permute-$gain-$chans-$size"
+          dst="$src.$tag.mov"
 
-        if [ "$src" -nt "$dst" ]; then
-          echo "$src -> $dst"
-          tmp="$dst.tmp.mov"
-          dt_config="--permute $permute --gain $gain"
-          [ "$chans" = 'y' ] && dt_config="$dt_config --mono"
+          if [ "$src" -nt "$dst" ]; then
 
-          ffmpeg -i "$src" -pix_fmt yuv444p -s 256x256 -t $DURATION -f yuv4mpegpipe - \
-            | ./downtown $dt_config \
-            | ffmpeg \
-                -f yuv4mpegpipe -i - \
-                -t $DURATION -i "$src" \
-                -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
-                -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst"
+            tmp="$dst.tmp.mov"
 
-        fi
+            dt_config="--size $outsize --permute $permute --gain $gain"
+            [ "$chans" = 'y' ] && dt_config="$dt_config --mono"
 
+            echo "$src -> $dst ($dt_config)"
+
+            ffmpeg -i "$src" -pix_fmt yuv444p -s ${size}x${size} -t $DURATION -f yuv4mpegpipe - \
+              | ./downtown $dt_config \
+              | ffmpeg \
+                  -f yuv4mpegpipe -i - \
+                  -t $DURATION -i "$src" \
+                  -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
+                  -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst"
+
+          fi
+
+        done
       done
     done
   done
