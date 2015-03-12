c24d3496ef8768aab921664b16afd55271724708
Andy Armstrong <andy@hexten.net>
2015-03-12 12:43:00 +0000

    Moar tweakin'

--- tools/pop.sh
+++ tools/pop.sh
@@ -3,12 +3,21 @@
 DURATION=240
 
 outsize="1920x1080"
+merge=5
 
 for src in "$@"; do
-  for gain in 1 4 16; do
-    for chans in 'y' 'yuv'; do
-      for size in 64 128 256 512; do
-        for permute in 'zigzag' 'raster' 'weave'; do
+
+  raw="$src.raw.y4m2"
+  rm -rf "$raw"
+
+  for size in 256; do
+
+    scaled="$src.$size.y4m2"
+    rm -rf "$scaled"
+
+    for gain in 10; do
+      for chans in 'y'; do
+        for permute in 'zigzag'; do
 
           tag="$permute-$gain-$chans-$size"
           dst="$src.$tag.mov"
@@ -17,27 +26,44 @@ for src in "$@"; do
 
             tmp="$dst.tmp.mov"
 
+            if [ ! -e "$raw" ]; then
+              dt_f_config="--merge $merge"
+              ffmpeg -nostdin -i "$src" -t $DURATION -f yuv4mpegpipe - \
+                | ./downtown_filter $dt_f_config > "$raw"
+            fi
+
+            if [ ! -e "$scaled" ]; then
+              ffmpeg -nostdin -f yuv4mpegpipe -i "$raw" -pix_fmt yuv444p -s ${size}x${size} -f yuv4mpegpipe -y "$scaled"
+            fi
+
             dt_config="--size $outsize --permute $permute --gain $gain"
             [ "$chans" = 'y' ] && dt_config="$dt_config --mono"
 
             echo "$src -> $dst ($dt_config)"
 
-            ffmpeg -i "$src" -pix_fmt yuv444p -s ${size}x${size} -t $DURATION -f yuv4mpegpipe - \
-              | ./downtown $dt_config \
-              | ffmpeg \
-                  -f yuv4mpegpipe -i - \
-                  -t $DURATION -i "$src" \
-                  -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
-                  -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst"
+            # Pipe source via downtown_filter into two pipes; one for downtown, one for the overlay
+
+            ffmpeg \
+              -nostdin \
+              -f yuv4mpegpipe \
+              -i <( cat "$scaled" | ./downtown $dt_config ) \
+              -i "$raw" \
+              -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
+              -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst" || exit
 
           fi
 
-        done
-      done
-    done
-  done
+        done #permute
+      done #chans
+    done #gain
 
-done
+    rm -rf "$scaled"
+
+  done #size
+
+  rm -rf "$raw"
+
+done #src
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
