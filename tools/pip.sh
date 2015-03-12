d50089893509cc5ec73cc067adac1c788a8c86c0
Andy Armstrong <andy@hexten.net>
2015-03-12 03:43:47 +0000

    Picture in picture.

---
+++ tools/pip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DURATION=120
+
+for src in "$*"; do
+  dst="$src.downtown.mov"
+  if [ "$src" -nt "$dst" ]; then
+    tmp="$dst.tmp.mov"
+
+    ffmpeg -i "$src" -pix_fmt yuv420p -s 256x256 -t $DURATION -f yuv4mpegpipe - \
+      | ./downtown \
+      | ffmpeg \
+          -f yuv4mpegpipe -i - \
+          -t $DURATION -i "$src" \
+          -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
+          -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst"
+
+  fi
+done
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
