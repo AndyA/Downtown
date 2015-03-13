18e7e1d0b1b955361baca5b1b82a6db23ef6009e
Andy Armstrong <andy@hexten.net>
2015-03-13 01:57:38 +0000

    Handle delta/nodelta

--- tools/pop.sh
+++ tools/pop.sh
@@ -18,41 +18,44 @@ for src in "$@"; do
     for gain in 5; do
       for chans in 'y'; do
         for permute in 'zigzag'; do
+          for delta in 'delta'; do
 
-          tag="$permute-$gain-$chans-$size"
-          dst="$src.$tag.mov"
+            tag="$permute-$gain-$chans-$size-$delta"
+            dst="$src.$tag.mov"
 
-          if [ "$src" -nt "$dst" ]; then
+            if [ "$src" -nt "$dst" ]; then
 
-            tmp="$dst.tmp.mov"
+              tmp="$dst.tmp.mov"
 
-            if [ ! -e "$raw" ]; then
-              dt_f_config="--merge $merge"
-              ffmpeg -nostdin -i "$src" -t $DURATION -f yuv4mpegpipe - \
-                | ./downtown_filter $dt_f_config > "$raw"
+              if [ ! -e "$raw" ]; then
+                dt_f_config="--merge $merge"
+                ffmpeg -nostdin -i "$src" -t $DURATION -f yuv4mpegpipe - \
+                  | ./downtown_filter $dt_f_config > "$raw"
+              fi
+
+              if [ ! -e "$scaled" ]; then
+                ffmpeg -nostdin -f yuv4mpegpipe -i "$raw" -pix_fmt yuv444p -s ${size}x${size} -f yuv4mpegpipe -y "$scaled"
+              fi
+
+              dt_config="--size $outsize --permute $permute --gain $gain"
+              [ "$chans" = 'y' ] && dt_config="$dt_config --mono"
+              [ "$delta" = 'delta' ] && dt_config="$dt_config --delta"
+
+              echo "$src -> $dst ($dt_config)"
+
+              # Pipe source via downtown_filter into two pipes; one for downtown, one for the overlay
+
+              ffmpeg \
+                -nostdin \
+                -f yuv4mpegpipe \
+                -i <( cat "$scaled" | ./downtown $dt_config ) \
+                -i "$raw" \
+                -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
+                -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst" || exit
+
             fi
 
-            if [ ! -e "$scaled" ]; then
-              ffmpeg -nostdin -f yuv4mpegpipe -i "$raw" -pix_fmt yuv444p -s ${size}x${size} -f yuv4mpegpipe -y "$scaled"
-            fi
-
-            dt_config="--size $outsize --permute $permute --gain $gain"
-            [ "$chans" = 'y' ] && dt_config="$dt_config --mono"
-
-            echo "$src -> $dst ($dt_config)"
-
-            # Pipe source via downtown_filter into two pipes; one for downtown, one for the overlay
-
-            ffmpeg \
-              -nostdin \
-              -f yuv4mpegpipe \
-              -i <( cat "$scaled" | ./downtown $dt_config ) \
-              -i "$raw" \
-              -filter_complex '[0:v][1:v]overlay=x=30:y=40[out]' -map '[out]' \
-              -aspect 16:9 -c:v libx264 -b:v 8000k -y "$tmp" && mv "$tmp" "$dst" || exit
-
-          fi
-
+          done #delta
         done #permute
       done #chans
     done #gain
