1501b80d7d28ade9cd58c3698da6953956d741c2
Andy Armstrong <andy@hexten.net>
2015-03-18 20:22:41 +0000

    More testing &c.

--- test/MovieMakerSpec.js
+++ test/MovieMakerSpec.js
@@ -1,5 +1,12 @@
-var expect = require("chai").expect;
+"use strict";
+
+var chai = require("chai");
+chai.use(require('chai-subset'));
+
+var expect = chai.expect;
+
 var MM = require("../lib/MovieMaker.js");
+var Canvas = require("canvas");
 
 describe("MovieMaker", function() {
 
@@ -214,6 +221,35 @@ describe("MovieMaker", function() {
 
       expect(stuff).to.deep.equal(want);
 
+    });
+
+    it("shouldn't mind if params is null", function() {
+      function render(canvas, ctx, framenum) {}
+      var c = new MM.Clip(render, 100);
+      c.bindParameters(null, {});
+    });
+
+  });
+
+  describe("TransformClip", function() {
+
+    it("should update context", function() {
+
+      var canvas = new Canvas(10, 10);
+      var ctx = canvas.getContext('2d');
+
+      var ctx_trans = null;
+      function render(canvas, ctx, framenum) {
+        console.log('currentTransform=' + ctx.currentTransform);
+        ctx_trans = ctx.currentTransform;
+      }
+      var clip = new MM.Clip(render, 100);
+
+      var transform = new MM.TransformClip(clip);
+
+      transform.makeFrame(canvas, ctx, 0);
+
+      // Would be nice to think of something to assert
     });
 
   });
