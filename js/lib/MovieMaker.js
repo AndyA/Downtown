fff252d3e90a0b9b688e46c7defd44d950b48385
Andy Armstrong <andy@hexten.net>
2015-03-18 08:27:04 +0000

    Dynamic properties.

--- js/lib/MovieMaker.js
+++ js/lib/MovieMaker.js
@@ -4,66 +4,111 @@ module.exports = (function() {
 
   var exports = {};
 
+  function DynamicPropertyBase() {}
+
+  function ClipBase() {}
+
+  ClipBase.prototype.clearPropertyCache = function() {
+    delete this._prop_cache;
+  }
+
+  ClipBase.prototype.bindProperty = function(name, prop) {
+    if (prop instanceof DynamicPropertyBase) {
+      this.bindProperty(name, function(framenum, portion) {
+        return prop.evaluate(framenum, portion);
+      });
+      return;
+    }
+
+    if (prop instanceof Function) {
+      Object.defineProperty(this, name, {
+        get: function() {
+          if (!this._prop_cache) this._prop_cache = {};
+          if (this._prop_cache.hasOwnProperty(name)) return this._prop_cache[name];
+          var framenum = this._current_frame;
+          var nframes = this.getFrames(); // cache?
+          return this._prop_cache[name] = prop(framenum, framenum / nframes);
+        }
+      });
+    } else {
+      Object.defineProperty(this, name, {
+        get: function() {
+          return prop
+        }
+      });
+    }
+  }
+
+  ClipBase.prototype.makeFrame = function(canvas, ctx, framenum) {
+    this.clearPropertyCache();
+    this._current_frame = framenum;
+    this.render(canvas, ctx, framenum);
+  }
+
+  // Simple rendered clip
   exports.Clip = function(render, frames) {
     this.render = render;
     this.frames = frames;
   }
 
-  exports.Clip.prototype = {
-    getFrames: function() {
-      return this.frames;
-    }
+  exports.Clip.prototype = new ClipBase();
+
+  exports.Clip.prototype.getFrames = function() {
+    return this.frames;
   }
 
+  // Reverse clip
   exports.ReverseClip = function(clip) {
     this.clip = clip;
     this.frames = clip.getFrames();
   }
 
-  exports.ReverseClip.prototype = {
-    render: function(canvas, ctx, framenum) {
-      this.clip.render(canvas, ctx, this.frames - 1 - framenum);
-    },
+  exports.ReverseClip.prototype = new ClipBase();
 
-    getFrames: function() {
-      return this.frames;
-    }
+  exports.ReverseClip.prototype.render = function(canvas, ctx, framenum) {
+    this.clip.makeFrame(canvas, ctx, this.frames - 1 - framenum);
   }
 
+  exports.ReverseClip.prototype.getFrames = function() {
+    return this.frames;
+  }
+
+  // A sequence of clips
   exports.SequenceClip = function() {
     this.clips = [];
     for (var i = 0; i < arguments.length; i++) {
       this.clips.push(arguments[i]);
     }
+
   }
 
-  exports.SequenceClip.prototype = {
-    append: function() {
-      for (var i = 0; i < arguments.length; i++) {
-        this.clips.push(arguments[i]);
-      }
-    },
+  exports.SequenceClip.prototype = new ClipBase();
 
-    render: function(canvas, ctx, framenum) {
-      var clips = this.clips;
-      for (var i = 0; i < clips.length; i++) {
-        var clip_frames = clips[i].getFrames();
-        if (framenum < clip_frames) {
-          clips[i].render(canvas, ctx, framenum);
-          return;
-        }
-        framenum -= clip_frames;
+  exports.SequenceClip.prototype.render = function(canvas, ctx, framenum) {
+    var clips = this.clips;
+    for (var i = 0; i < clips.length; i++) {
+      var clip_frames = clips[i].getFrames();
+      if (framenum < clip_frames) {
+        clips[i].makeFrame(canvas, ctx, framenum);
+        return;
       }
-    },
+      framenum -= clip_frames;
+    }
+  }
 
-    getFrames: function() {
-      var frames = 0;
-      var clips = this.clips;
-      for (var i = 0; i < clips.length; i++) {
-        frames += clips[i].getFrames();
+  exports.SequenceClip.prototype.getFrames = function() {
+    var frames = 0;
+    var clips = this.clips;
+    for (var i = 0; i < clips.length; i++) {
+      frames += clips[i].getFrames();
 
-      }
-      return frames;
+    }
+    return frames;
+  }
+
+  exports.SequenceClip.prototype.append = function() {
+    for (var i = 0; i < arguments.length; i++) {
+      this.clips.push(arguments[i]);
     }
   }
 
@@ -74,30 +119,29 @@ module.exports = (function() {
     }
   }
 
-  exports.OverlayClip.prototype = {
+  exports.OverlayClip.prototype = new ClipBase();
 
-    render: function(canvas, ctx, framenum) {
-      var clips = this.clips;
-      for (var i = 0; i < clips.length; i++) {
-        ctx.save();
-        clips[i].render(canvas, ctx, framenum);
-        ctx.restore();
-      }
-    },
-
-    getFrames: function() {
-      var frames = null;
-      var clips = this.clips;
-
-      for (var i = 0; i < clips.length; i++) {
-        var clip_frames = clips[i].getFrames();
-        if (clip_frames !== null && (frames === null || clip_frames < frames)) {
-          frames = clip_frames;
-        }
-
-      }
-      return frames;
+  exports.OverlayClip.prototype.render = function(canvas, ctx, framenum) {
+    var clips = this.clips;
+    for (var i = 0; i < clips.length; i++) {
+      ctx.save();
+      clips[i].makeFrame(canvas, ctx, framenum);
+      ctx.restore();
     }
+  }
+
+  exports.OverlayClip.prototype.getFrames = function() {
+    var frames = null;
+    var clips = this.clips;
+
+    for (var i = 0; i < clips.length; i++) {
+      var clip_frames = clips[i].getFrames();
+      if (clip_frames !== null && (frames === null || clip_frames < frames)) {
+        frames = clip_frames;
+      }
+
+    }
+    return frames;
   }
 
   exports.EditClip = function(clip, in_frame, frames) {
@@ -110,60 +154,58 @@ module.exports = (function() {
     this.frames = frames;
   }
 
-  exports.EditClip.prototype = {
+  exports.EditClip.prototype = new ClipBase();
 
-    render: function(canvas, ctx, framenum) {
-      this.clip.render(canvas, ctx, framenum + this.in_frame);
-    },
+  exports.EditClip.prototype.render = function(canvas, ctx, framenum) {
+    this.clip.makeFrame(canvas, ctx, framenum + this.in_frame);
+  }
 
-    getFrames: function() {
-      return this.frames;
-    }
+  exports.EditClip.prototype.getFrames = function() {
+    return this.frames;
   }
 
   exports.TimecodeClip = function() {}
+  exports.TimecodeClip.prototype = new ClipBase();
 
-  exports.TimecodeClip.prototype = {
-    render: function(canvas, ctx, framenum) {
-      ctx.font = "18px monospace";
-      ctx.fillStyle = 'green';
-      ctx.fillText(framenum, 100, 100);
-      //    ctx.fillRect(framenum, 0, 10, canvas.height);
-    },
+  exports.TimecodeClip.prototype.render = function(canvas, ctx, framenum) {
+    ctx.font = "18px monospace";
+    ctx.fillStyle = 'green';
+    ctx.fillText(framenum, 100, 100);
+  }
 
-    getFrames: function() {
-      return null; // unlimited
-    }
+  exports.TimecodeClip.prototype.getFrames = function() {
+    return null; // unlimited
   }
 
   exports.TitleClip = function(text, frames, x, y, setStyle) {
     this.text = text;
     this.frames = frames;
-    this.x = x || 0.5;
-    this.y = y || 0.5;
+
+    this.bindProperty('x', arguments.length > 2 ? x : 0.5);
+    this.bindProperty('y', arguments.length > 3 ? y : 0.5);
 
     this.setStyle = setStyle;
   }
 
-  exports.TitleClip.prototype = {
-    render: function(canvas, ctx, framenum) {
-      ctx.textAlign = 'left';
-      ctx.textBaseline = 'alphabetic';
-      ctx.font = "80px sans-serif";
-      ctx.fillStyle = 'white';
-      if (this.setStyle) this.setStyle(ctx);
-      var metrics = ctx.measureText(this.text);
-      var height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+  exports.TitleClip.prototype = new ClipBase();
 
-      var dw = canvas.width - metrics.width;
-      var dh = canvas.height - height;
+  exports.TitleClip.prototype.render = function(canvas, ctx, framenum) {
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'alphabetic';
+    ctx.font = "80px sans-serif";
+    ctx.fillStyle = 'white';
+    if (this.setStyle) this.setStyle(ctx);
+    var metrics = ctx.measureText(this.text);
+    var height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-      ctx.fillText(this.text, dw * this.x - metrics.actualBoundingBoxLeft, dh * this.y - metrics.actualBoundingBoxDescent);
-    },
+    var dw = canvas.width - metrics.width;
+    var dh = canvas.height - height;
 
-    getFrames: function() {
-      return this.frames;
-    }
+    ctx.fillText(this.text, dw * this.x - metrics.actualBoundingBoxLeft, dh * this.y - metrics.actualBoundingBoxDescent);
+  }
+
+  exports.TitleClip.prototype.getFrames = function() {
+    return this.frames;
   }
 
   exports.DisolveClip = function(clip_a, clip_b, frames, easing) {
@@ -181,92 +223,90 @@ module.exports = (function() {
     if (this.frames > this.frames_b) this.frames = this.frames_b;
   }
 
-  exports.DisolveClip.prototype = {
-    render: function(canvas, ctx, framenum) {
-      var start = this.frames_a - this.frames;
-      if (framenum < start) {
-        this.clip_a.render(canvas, ctx, framenum);
-      } else if (framenum < this.frames_a) {
-        var mix = this.easing((framenum - start) / this.frames);
+  exports.DisolveClip.prototype = new ClipBase();
 
-        ctx.save();
+  exports.DisolveClip.prototype.render = function(canvas, ctx, framenum) {
+    var start = this.frames_a - this.frames;
+    if (framenum < start) {
+      this.clip_a.makeFrame(canvas, ctx, framenum);
+    } else if (framenum < this.frames_a) {
+      var mix = this.easing((framenum - start) / this.frames);
 
-        // Draw clip_a
-        var alpha = ctx.globalAlpha;
+      ctx.save();
 
-        ctx.globalAlpha = alpha * (1 - mix);
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        this.clip_a.render(canvas, ctx, framenum);
+      // Draw clip_a
+      var alpha = ctx.globalAlpha;
 
-        ctx.restore();
+      ctx.globalAlpha = alpha * (1 - mix);
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      this.clip_a.makeFrame(canvas, ctx, framenum);
 
-        // Overlay clip_b
-        ctx.globalAlpha = alpha * mix;
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        this.clip_b.render(canvas, ctx, framenum - start);
+      ctx.restore();
 
-      } else {
-        this.clip_b.render(canvas, ctx, framenum - start);
-      }
+      // Overlay clip_b
+      ctx.globalAlpha = alpha * mix;
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      this.clip_b.makeFrame(canvas, ctx, framenum - start);
 
-    },
-
-    getFrames: function() {
-      return this.frames_a - this.frames + this.frames_b;
+    } else {
+      this.clip_b.makeFrame(canvas, ctx, framenum - start);
     }
+  }
+
+  exports.DisolveClip.prototype.getFrames = function() {
+    return this.frames_a - this.frames + this.frames_b;
   }
 
   exports.BlankClip = function(frames) {
-    this.frames = frames;
+    this.frames = frames
   }
 
-  exports.BlankClip.prototype = {
-    render: function(canvas, ctx, framenum) {},
-
-    getFrames: function() {
-      return this.frames;
-    }
+  exports.BlankClip.prototype = new ClipBase();
+  exports.BlankClip.prototype.render = function(canvas, ctx, framenum) {}
+  exports.BlankClip.prototype.getFrames = function() {
+    return this.frames;
   }
 
   exports.MovieStream = function(out, width, height, clip) {
     this.init(out, width, height, clip);
   }
 
-  exports.MovieStream.prototype = {
-    init: function(out, width, height, clip) {
-      this.out = out;
-      this.width = width;
-      this.height = height;
-      this.clip = clip;
-    },
-    render: function() {
+  exports.MovieStream.prototype = new Object();
 
-      var framenum = 0;
-      var frames = this.clip.getFrames();
-      var self = this;
+  exports.MovieStream.prototype.init = function(out, width, height, clip) {
+    this.out = out;
+    this.width = width;
+    this.height = height;
+    this.clip = clip;
+  }
 
-      function drawFrame() {
-        var canvas = new Canvas(self.width, self.height);
-        var ctx = canvas.getContext('2d');
-        console.log("Rendering frame " + framenum);
-        ctx.save();
-        self.clip.render(canvas, ctx, framenum);
-        ctx.restore();
-        var stream = canvas.createJPEGStream();
+  exports.MovieStream.prototype.render = function() {
 
-        stream.on('data', function(chunk) {
-          self.out.write(chunk);
-        });
+    var framenum = 0;
+    var frames = this.clip.getFrames();
+    var self = this;
 
-        stream.on('end', function() {
-          if (++framenum < frames) drawFrame();
-        });
-      }
+    function drawFrame() {
+      var canvas = new Canvas(self.width, self.height);
+      var ctx = canvas.getContext('2d');
+      console.log("Rendering frame " + framenum);
+      ctx.save();
+      self.clip.makeFrame(canvas, ctx, framenum);
+      ctx.restore();
+      var stream = canvas.createJPEGStream();
 
-      drawFrame();
+      stream.on('data', function(chunk) {
+        self.out.write(chunk);
+      });
+
+      stream.on('end', function() {
+        if (++framenum < frames) drawFrame();
+      });
     }
+
+    drawFrame();
   }
 
   exports.MovieMaker = function(filename, width, height, clip) {
