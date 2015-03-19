03efaab0ce8ad63e95369c3fe975ec0401ff18d9
Andy Armstrong <andy@hexten.net>
2015-03-19 00:33:44 +0000

    Check for render, getFrames.

--- test/MovieMakerSpec.js
+++ test/MovieMakerSpec.js
@@ -431,6 +431,37 @@ describe("MovieMaker", function() {
 
   });
 
+  (function() {
+    var clip_types = {
+      "BlankClip": MM.BlankClip,
+      "DisolveClip": MM.DisolveClip,
+      "EditClip": MM.EditClip,
+      "OverlayClip": MM.OverlayClip,
+      "ReverseClip": MM.ReverseClip,
+      "SequenceClip": MM.SequenceClip,
+      "TimecodeClip": MM.TimecodeClip,
+      "TitleClip": MM.TitleClip,
+      "TransformClip": MM.TransformClip,
+    };
+
+    var clip_names = Object.keys(clip_types);
+
+    for (var i = 0; i < clip_names.length; i++) {
+      var name = clip_names[i];
+      var klass = clip_types[name];
+
+      describe(name, function() {
+
+        it("supports standard methods", function() {
+          var clip = new klass();
+          expect(clip).to.respondTo('render').and.respondTo('getFrames');
+        });
+
+      });
+
+    }
+  })()
+
   describe("TransformClip", function() {
 
     it("should update context", function() {
