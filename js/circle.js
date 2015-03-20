8639581910125152605a2315c914cbf17017a861
Andy Armstrong <andy@hexten.net>
2015-03-20 00:11:14 +0000

    Wibble.

--- js/circle.js
+++ js/circle.js
@@ -8,14 +8,16 @@ var HEIGHT = 1080;
 var RADIUS = Math.min(WIDTH, HEIGHT)
 
 var wibbler = new MM.CircleProperty({
-  frequency: 7
+  frequency: 7,
+  amplitute: RADIUS / 10
 });
 
 var circle = new MM.CircleProperty({
-  amplitute: RADIUS / 4,
+  amplitute: function() {
+    return RADIUS / 4 + wibbler.x
+  },
   offset_x: 0.5,
-  offset_y: 0.5,
-  scale_x: wibbler.x
+  offset_y: 0.5
 });
 
 var title = new MM.TitleClip("Circle", 500, 0.5, 0.5);
