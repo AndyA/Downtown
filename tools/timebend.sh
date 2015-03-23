3840e55097942736e3624766bf99b9732593f859
Andy Armstrong <andy@hexten.net>
2015-03-23 10:33:55 +0000

    Check filt.

--- tools/timebend.sh
+++ tools/timebend.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+FILT="./test-timebend"
+
 for src in "$@"; do
   dst="$src.timebend.mov"
   tmp="$dst.tmp.mov"
-  if [ "$src" -nt "$dst" ]; then
+  if [ "$src" -nt "$dst" ] || [ "$FILT" -nt "$dst" ]; then
     ffmpeg -nostdin -i "$src" -f yuv4mpegpipe - \
-      | ./test-timebend \
+      | "$FILT" \
       | ffmpeg -nostdin \
           -f yuv4mpegpipe -i - \
           -pix_fmt yuv420p -c:v libx264 -b:v 4000k \
