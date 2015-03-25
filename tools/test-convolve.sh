48e101b0edc0350f31dd1267f41b4095df156ef5
Andy Armstrong <andy@hexten.net>
2015-03-25 13:39:35 +0000

    Don't do prescale.

--- tools/test-convolve.sh
+++ tools/test-convolve.sh
@@ -8,18 +8,16 @@ rm -rf "$graph"
 
 tools/unity.pl t/data/kernel.*.dat
 
-for prescale in 0.1 1 10; do
-  for kernel in t/data/kernel.*.unity; do
-    for data in t/data/data.*.dat; do
-      kn="$( basename "$kernel" .unity )"
-      dn="$( basename "$data" .dat )"
-      tag="$kn.$dn.$prescale"
-      echo "$tag"
-      set -x
-      ./test-convolve -p $prescale -c $count -o "$graph/$tag.%03d.dat" "$data" "$kernel" || exit
-      ./tools/mk-graph.pl "$graph/$tag.png" "$graph"/"$tag".*.dat | gnuplot || exit
-      set +x
-    done
+for kernel in t/data/kernel.*.unity; do
+  for data in t/data/data.*.dat; do
+    kn="$( basename "$kernel" .unity )"
+    dn="$( basename "$data" .dat )"
+    tag="$kn.$dn"
+    echo "$tag"
+    set -x
+    ./test-convolve -c $count -o "$graph/$tag.%03d.dat" "$data" "$kernel" || exit
+    ./tools/mk-graph.pl "$graph/$tag.png" "$graph"/"$tag".*.dat | gnuplot || exit
+    set +x
   done
 done
 
