60daa6f95bd4bf4cd2eb2121e73fdf5f8b8541b6
Andy Armstrong <andy@hexten.net>
2015-03-25 13:39:17 +0000

    Scale according to first series.

--- tools/mk-graph.pl
+++ tools/mk-graph.pl
@@ -7,16 +7,24 @@ use strict;
 use warnings;
 
 my ( $out, @dat ) = @ARGV;
+my $first = $dat[0];
 
 die "Nothing to plot" unless @dat;
 
 print <<EOS;
-set term png size 1920,1080
 set bmargin 8
-set output "$out"
 set key left
 
 set offsets graph 0, 0, 0.05, 0.05
+
+set term unknown
+
+plot "$first" with lines title '$first'
+
+set yr [-0.1:GPVAL_DATA_Y_MAX*1.1]
+
+set term png size 1920,1080
+set output "$out"
 
 EOS
 
