572b18e2610e18e1af8701d09e7d1e6b9eda9b87
Andy Armstrong <andy@hexten.net>
2015-04-01 15:02:21 +0100

    More test coverage for Average.

--- test/Downtown/Average.js
+++ test/Downtown/Average.js
@@ -5,7 +5,30 @@ var expect = chai.expect;
 
 var Average = require("../../lib/Downtown/Average.js");
 
+function computeAverage(fn, from, to) {
+  var total = 0;
+  var min = NaN;
+  var max = NaN;
+
+  for (var i = from; i <= to; i++) {
+    var datum = fn(i);
+    if (isNaN(min) || datum < min) min = datum;
+    if (isNaN(max) || datum > max) max = datum;
+    total += datum;
+  }
+
+  return {
+    min: min,
+    max: max,
+    avg: total / (to - from + 1)
+  }
+}
+
 describe("Average", function() {
+
+  function xn(n) {
+    return ((100 - n) * 3.5 * ((n & 1) ? -1.3 : 3.2));
+  }
 
   describe("Basic", function() {
 
@@ -46,6 +69,32 @@ describe("Average", function() {
 
     });
 
+  });
+
+  describe("Soak test", function() {
+
+    it("should return sensible values", function() {
+
+      for (var len = 1; len < 5; len++) {
+        var a = new Average(len);
+
+        for (var x = 0; x < len * 2; x++) {
+          var datum = xn(x);
+          a.add(datum);
+          var got_avg = a.average();
+          var got_min = a.min();
+          var got_max = a.max();
+
+          var used = Math.min(x + 1, len);
+          var want = computeAverage(xn, x - used + 1, x);
+
+          expect(a.length()).to.equal(used);
+          expect(got_avg).to.be.closeTo(want.avg, 0.00001);
+          expect(got_min).to.be.closeTo(want.min, 0.00001);
+          expect(got_max).to.be.closeTo(want.max, 0.00001);
+        }
+      }
+    });
   });
 
 });
